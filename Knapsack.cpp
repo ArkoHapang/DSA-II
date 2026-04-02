5876762d61632d6318fed5e80b4c10cdd62f3fe4
Arko Hapang <0802420205101007@baust.edu.bd>
2026-04-02 20:23:38 +0600

    Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -1,68 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 struct Item {
-    // Fill it yourself
+    int id;
+    double weight;
+    double profit;
+    double ratio; // Value per unit weight
 };
-
 bool compare(Item a, Item b) {
-    // TODO: Sort in descending order of ratio
-    return ____________________;
+    return a.ratio > b.ratio;
 }
-
 void fractionalKnapsack(vector<Item>& items, double capacity) {
-
-    // Step 1: Compute ratio
     for (auto &item : items) {
         item.ratio = item.profit / item.weight;
     }
-
-    // Step 2: Sort items
     sort(items.begin(), items.end(), compare);
-
     double totalProfit = 0.0;
-
-    cout << "Selected Items:\n";
-
-    // Step 3: Take items greedily
+     cout << "Selected Items:\n";
     for (auto item : items) {
-
-        if (capacity >= item.weight) {
-
-            // Take full item
+        if (capacity <= 0) break; 
+         if (capacity >= item.weight) {
             capacity -= item.weight;
             totalProfit += item.profit;
-
             cout << "Item " << item.id << " (Full)\n";
         }
         else {
-            // Take fractional part
             totalProfit += capacity * item.ratio;
-
-            cout << "Item " << item.id
-                 << " (Fractional, Weight taken = "
+             cout << "Item " << item.id 
+                 << " (Fractional, Weight taken = " 
                  << capacity << ")\n";
 
-            capacity = 0;
-            break;   // Knapsack is full
+            capacity = 0; 
+            break;    
         }
     }
-
-    cout << "Total Profit: " << totalProfit << endl;
+     cout << "Total Profit: " << totalProfit << endl;
 }
-
 int main() {
-
     vector<Item> items = {
-        {1, 10, 60, 0},
-        {2, 20, 100, 0},
-        {3, 30, 120, 0}
+        {1, 10.0, 60.0, 0.0},
+        {2, 20.0, 100.0, 0.0},
+        {3, 30.0, 120.0, 0.0}
     };
-
     double capacity = 50.0;
-
     fractionalKnapsack(items, capacity);
-
     return 0;
 }
