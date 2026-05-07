b5a9b40c184130bf8e244a091eb22ab0bf94f841
Arko Hapang <0802420205101007@baust.edu.bd>
2026-05-08 00:58:47 +0600

    RodCutting.cpp

--- RodCutting.cpp
+++ RodCutting.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int price[100];
+
+int rc(int i, int n) {
+    if (n == 0 || i > n) return 0;
+
+    int skip = rc(i + 1, n);
+    int take = price[i] + rc(i, n - 1);
+
+    return max(take, skip);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> price[i];
+
+    cout << rc(1, n);
+    return 0;
+}
 
